f246de70c69ec9134e22ba054df5315e69db0b9b
Ray-Hackshaw <ray.hackshaw@gmail.com>
2024-11-27 21:46:35 +1300

    test: import local font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Mona_Sans } from "next/font/google";
-
-const monaSans = Mona_Sans({
-  subsets: ["latin"],
-  variable: "--font-mona-sans",
-  display: "swap",
-});
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "MedCura",
   description: "Transforming Healthcare with Every Connection.",
 };
+
+const monaSans = localFont({
+  src: "./fonts/Mona-Sans.woff2",
+  variable: "--font-mona-sans",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
