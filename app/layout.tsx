382748537fec138fd871d76ef5927a02d9d172d5
Ray-Hackshaw <ray.hackshaw@gmail.com>
2024-11-27 21:28:32 +1300

    fix: build?

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Mona_Sans } from "next/font/google";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const monaSans = Mona_Sans({
   subsets: ["latin"],
@@ -32,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden ${monaSans.className}`}
-      >
+      <body className={`antialiased overflow-hidden ${monaSans.className}`}>
         {children}
       </body>
     </html>
